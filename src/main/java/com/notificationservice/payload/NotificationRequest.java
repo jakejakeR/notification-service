9fa3eda7bafb01f172161aa40d5bae5c8119a118
Jakub Rogowski <jakerogowski33gmail.com>
2022-12-05 10:21:54 +0100

    NotificationRequest typo

--- src/main/java/com/notificationservice/payload/NotificationRequest.java
+++ src/main/java/com/notificationservice/payload/NotificationRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class NotifcationRequest {
+public class NotificationRequest {
 
     private String from;
     private String to;
