49810e8c83fe7cd31c1353f88b454f804ea10891
Jakub Rogowski <jakerogowski33gmail.com>
2022-12-07 15:10:31 +0100

    Notification to record

--- src/main/java/com/notificationservice/model/Notification.java
+++ src/main/java/com/notificationservice/model/Notification.java
@@ -1,15 +1,4 @@
 package com.notificationservice.model;
 
-import lombok.Data;
-
-@Data
-public class Notification {
-
-    private final String from;
-    private final String to;
-    private final String message;
-
-    public static Notification of(String from, String to, String message) {
-        return new Notification(from, to, message);
-    }
+public record Notification(String from, String to, String message, String redirectTo) {
 }
