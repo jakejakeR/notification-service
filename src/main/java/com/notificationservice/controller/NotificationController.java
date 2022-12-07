829c066e24ae5cbd5bcb6cca0986f0ffd031195d
Jakub Rogowski <jakerogowski33gmail.com>
2022-12-07 17:26:45 +0100

    logger added

--- src/main/java/com/notificationservice/controller/NotificationController.java
+++ src/main/java/com/notificationservice/controller/NotificationController.java
@@ -3,9 +3,11 @@ package com.notificationservice.controller;
 import com.notificationservice.model.Notification;
 import com.notificationservice.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("https://localhost:3000")
@@ -16,6 +18,7 @@ public class NotificationController {
 
     @PostMapping
     public Mono<Notification> receiveNotification(@RequestBody Mono<Notification> notificationMono) {
+        log.info("Received notification: {}", notificationMono.toString());
         return service.receiveNotification(notificationMono);
     }
 }
